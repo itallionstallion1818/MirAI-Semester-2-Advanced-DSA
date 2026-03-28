78979db0990e6c57a202039515c004e26da7efaf
Aditya Prasad <adityaprasad@Adityas-MacBook-Air.local>
2026-03-28 14:02:42 +0530

    RatinMaze

---
+++ 28thMarch/RatinaMaze.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+void printsolution(int soln[][10],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<soln[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+bool ratinamaze(char maze[][5],int n,int i,int j,int soln[][10]){
+    if(i==n-1 && j==n-1){
+        soln[i][j]=1;
+        //Print the solution
+        printsolution(soln,n);
+        // Fool the recursion to print all possible outcomes.
+        return false;
+    }
+
+    soln[i][j]=1;
+    if(j+1<n && maze[i][j+1]!='X'){
+        bool solutionfromright=ratinamaze(maze,n,i,j+1,soln);
+        if(solutionfromright){
+            return true;
+        }
+    }
+    if(i+1<n && maze[i+1][j]!='X'){
+        bool solutionfromdown=ratinamaze(maze,n,i+1,j,soln);
+        if(solutionfromdown){
+            return true;
+        }
+    }
+    // Backtracking and re-setting the path as '0'
+    soln[i][j]=0;
+    return false;
+}
+
+int main(){
+
+    int n=4;
+    char maze[5][5];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>maze[i][j];
+        }
+    }
+    cout<<endl;
+    int soln[10][10]={0};
+    ratinamaze(maze,4,0,0,soln);
+    return 0;
+}
